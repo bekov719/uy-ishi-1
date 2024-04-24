af59865a5d5121409cf845f70fb409dc5b555637
bekov719 <odilbekhaitkulov@gmail.com>
2024-04-24 17:18:59 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let yosh = prompt (`yoshingizni kiriting`)
+let ism = prompt (`ismingizni kiriting`)
+let familiya = prompt (`familyangizni kiriting`)
+let maktab = prompt (`maktabingizni kiriting`)
+let manzil = prompt (`manzilingizni kiriting`)
+let telnomer = prompt (`raqamingizni kiriting`)
+let username = prompt (`usernameingizni kiriting`)
+let natija = "Salom" + yosh + "ismim" + familiya + "familiyam" + maktab + "maktabim" + ism + "ismim" + manzil + "manzilim" + telnomer + "raqamim" + username + "nikim" 
+console.log(natija)
+console.log(prompt)
